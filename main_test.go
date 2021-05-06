de043a64559b44e922d4e84f46ab0bd37f57d947
Andrey Slotin <andrey.slotin@instana.com>
2021-05-06 16:56:05 +0200

    Add go.mod

--- main_test.go
+++ main_test.go
@@ -10,8 +10,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/instana/testify/assert"
-	"github.com/instana/testify/require"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLookupInstanaSensor(t *testing.T) {
