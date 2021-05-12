39f8833d955a3c2f19e9b2309f8bcea057db80c9
Andrey Slotin <andrey.slotin@instana.com>
2021-05-12 16:27:44 +0200

    Instrument sql.Open() calls

---
+++ recipes/database_sql_test.go
@@ -0,0 +1,52 @@
+package recipes_test
+
+import (
+	"bytes"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"testing"
+
+	"github.com/instana/go-instana/recipes"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDatabaseSQLRecipe(t *testing.T) {
+	examples := map[string]struct {
+		TargetPkg string
+		Code      string
+		Expected  string
+	}{
+		"sql.Open": {
+			TargetPkg: "sql",
+			Code:      `sql.Open("sqlite", "sqlite:///var/data/db.sqlite")`,
+			Expected:  `instana.SQLOpen("sqlite", "sqlite:///var/data/db.sqlite")`,
+		},
+		"custom import": {
+			TargetPkg: "db",
+			Code:      `db.Open("sqlite", "sqlite:///var/data/db.sqlite")`,
+			Expected:  `instana.SQLOpen("sqlite", "sqlite:///var/data/db.sqlite")`,
+		},
+	}
+
+	for name, example := range examples {
+		t.Run(name, func(t *testing.T) {
+			node, err := parser.ParseExpr(example.Code)
+			require.NoError(t, err)
+
+			instrumented, changed := recipes.DatabaseSQL{
+				InstanaPkg: "instana",
+				TargetPkg:  example.TargetPkg,
+				SensorVar:  "__instanaSensor",
+			}.Instrument(node)
+
+			assert.True(t, changed)
+
+			buf := bytes.NewBuffer(nil)
+			require.NoError(t, format.Node(buf, token.NewFileSet(), instrumented))
+
+			assert.Equal(t, example.Expected, buf.String())
+		})
+	}
+}
