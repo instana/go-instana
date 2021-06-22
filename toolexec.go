434689ebee2e0b35f2cebbded9a06e249338eeab
Andrey Slotin <andrey.slotin@instana.com>
2021-06-22 17:00:42 +0200

    Parse $GOTOOLDIR/compile args to extract the list of paths to instrument

--- toolexec.go
+++ toolexec.go
@@ -4,9 +4,9 @@
 package main
 
 import (
-	"log"
-	"os"
+	"fmt"
 	"os/exec"
+	"strings"
 )
 
 // ParseToolchainCmd returns an exec.Cmd to execute the Go toolchain command
@@ -18,13 +18,53 @@ func ParseToolchainCmd(args []string) *exec.Cmd {
 	}
 
 	path, args := args[0], args[1:]
-
 	cmd := exec.Command(path, args...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	log.Println("tool: ", cmd)
 
 	return cmd
+}
+
+type toolchainCompileArgs struct {
+	Output  string
+	Package string
+	Files   []string
+}
+
+// Complete returns whether $GOTOOLDIR/compile has been called to compile a single package
+func (f toolchainCompileArgs) Complete() bool {
+	return f.Output != "" && f.Package != ""
+}
+
+// ParseToolchainCompileArgs parses the $GOTOOLDIR/compile args and extracts the list
+// of files to compile
+func ParseToolchainCompileArgs(args []string) (toolchainCompileArgs, error) {
+	var flags toolchainCompileArgs
+
+	for i := range args {
+		switch args[i] {
+		case "-o":
+			if i+1 >= len(args) || strings.HasPrefix(args[i+1], "-") {
+				return flags, fmt.Errorf("compile tool -o flag missing mandatory value")
+			}
+
+			flags.Output = args[i+1]
+			i++
+		case "-p":
+			if i+1 >= len(args) || strings.HasPrefix(args[i+1], "-") {
+				return flags, fmt.Errorf("compile tool -p flag missing mandatory value")
+			}
+
+			flags.Package = args[i+1]
+			i++
+		}
+	}
+
+	for i := len(args) - 1; i >= 0; i-- {
+		if !strings.HasSuffix(args[i], ".go") {
+			break
+		}
+
+		flags.Files = append(flags.Files, args[i])
+	}
+
+	return flags, nil
 }
