dceced3ddb256b7abe685322db66194f03a9aafa
Andrii Kushch <>
2022-05-30 11:48:51 +0200

    add tests for already instrumented code

--- recipes/lambda_test.go
+++ recipes/lambda_test.go
@@ -183,3 +183,105 @@ func assertInstrumentation(t *testing.T, examples map[string]struct {
 
 	}
 }
+
+func TestLambdaAlreadyInstrumented(t *testing.T) {
+
+	examples := map[string]struct {
+		TargetPkg string
+		Code      string
+	}{
+		"lambda.Start": {
+			TargetPkg: "lambda",
+			Code: `package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	instalambda "github.com/instana/go-sensor/instrumentation/instalambda"
+)
+
+func main() {
+	lambda.Start(instalambda.NewHandler(func() {
+	}, __instanaSensor))
+}
+`,
+		},
+		"lambda.StartHandler": {
+			TargetPkg: "lambda",
+			Code: `package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	instalambda "github.com/instana/go-sensor/instrumentation/instalambda"
+)
+
+func main() {
+	lambda.StartHandler(instalambda.WrapHandler(lambda.NewHandler(func() {
+	}), __instanaSensor))
+}
+`,
+		},
+		"lambda.StartHandlerWithContext": {
+			TargetPkg: "lambda",
+			Code: `package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+	instalambda "github.com/instana/go-sensor/instrumentation/instalambda"
+)
+
+func main() {
+	lambda.StartHandlerWithContext(context.Background(), instalambda.WrapHandler(lambda.NewHandler(func() {
+	}), __instanaSensor))
+}
+`,
+		},
+		"lambda.StartWithOptions": {
+			TargetPkg: "lambda",
+			Code: `package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+	instalambda "github.com/instana/go-sensor/instrumentation/instalambda"
+)
+
+func main() {
+	lambda.StartWithOptions(instalambda.NewHandler(func() {
+	}, __instanaSensor), lambda.WithContext(context.Background()))
+}
+`,
+		},
+		"lambda.StartWithContext": {
+			TargetPkg: "lambda",
+			Code: `package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+	instalambda "github.com/instana/go-sensor/instrumentation/instalambda"
+)
+
+func main() {
+	lambda.StartWithContext(context.Background(), instalambda.NewHandler(func() {
+	}, __instanaSensor))
+}
+`,
+		},
+	}
+
+	for name, example := range examples {
+
+		t.Run(name, func(t *testing.T) {
+			fset := token.NewFileSet()
+			node, err := parser.ParseFile(fset, "test", example.Code, parser.AllErrors)
+
+			require.NoError(t, err)
+
+			_, changed := recipes.NewLambda().
+				Instrument(token.NewFileSet(), node, example.TargetPkg, "__instanaSensor")
+
+			assert.False(t, changed)
+		})
+	}
+}
