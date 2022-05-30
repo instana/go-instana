8c9d67fcf6a7139f3a191a97a9072f44720157e6
Andrii Kushch <>
2022-05-30 11:44:46 +0200

    add basic lambda auto instrumentation

---
+++ recipes/lambda.go
@@ -0,0 +1,149 @@
+// (c) Copyright IBM Corp. 2022
+
+package recipes
+
+import (
+	"github.com/instana/go-instana/registry"
+	"go/ast"
+	"go/token"
+	"golang.org/x/tools/go/ast/astutil"
+	"log"
+)
+
+func init() {
+	registry.Default.Register("github.com/aws/aws-lambda-go", NewLambda())
+}
+
+func NewLambda() *Lambda {
+	return &Lambda{InstanaPkg: "instalambda"}
+}
+
+// Lambda instruments github.com/aws/aws-lambda-go package with Instana
+type Lambda struct {
+	InstanaPkg string
+}
+
+// ImportPath returns instrumentation import path
+func (recipe *Lambda) ImportPath() string {
+	return "github.com/instana/go-sensor/instrumentation/instalambda"
+}
+
+func (recipe *Lambda) Instrument(fset *token.FileSet, f ast.Node, targetPkg, sensorVar string) (result ast.Node, changed bool) {
+	result = astutil.Apply(f,
+		func(c *astutil.Cursor) bool {
+			return true
+		},
+		func(c *astutil.Cursor) bool {
+			switch node := c.Node().(type) {
+			case *ast.CallExpr:
+				changed = recipe.instrumentMethodCall(node, targetPkg, sensorVar) || changed
+			}
+
+			return true
+		},
+	)
+
+	if changed {
+		if val, ok := f.(*ast.File); ok {
+			log.Printf("AddNamedImport: %s %s", recipe.InstanaPkg, recipe.ImportPath())
+			astutil.AddNamedImport(fset, val, recipe.InstanaPkg, recipe.ImportPath())
+		}
+	}
+
+	return result, changed
+}
+
+func (recipe *Lambda) instrumentMethodCall(call *ast.CallExpr, targetPkg, sensorVar string) bool {
+	pkgName, fnName, ok := extractFunctionName(call)
+	if !ok {
+		return false
+	}
+
+	if pkgName != targetPkg {
+		return false
+	}
+
+	switch fnName {
+	case "Start":
+		if recipe.argumentsAlreadyInstrumented(call.Args, sensorVar) {
+			return false
+		}
+
+		call.Args[0] = recipe.instrumentationCallExpr("NewHandler", call.Args[0], sensorVar)
+
+		return true
+
+	case "StartHandler":
+		if recipe.argumentsAlreadyInstrumented(call.Args, sensorVar) {
+			return false
+		}
+
+		call.Args[0] = recipe.instrumentationCallExpr("WrapHandler", call.Args[0], sensorVar)
+
+		return true
+	case "StartHandlerWithContext":
+		if recipe.argumentsAlreadyInstrumented(call.Args, sensorVar) {
+			return false
+		}
+
+		call.Args[1] = recipe.instrumentationCallExpr("WrapHandler", call.Args[1], sensorVar)
+
+		return true
+	case "StartWithOptions":
+		if recipe.argumentsAlreadyInstrumented(call.Args, sensorVar) {
+			return false
+		}
+
+		call.Args[0] = recipe.instrumentationCallExpr("NewHandler", call.Args[0], sensorVar)
+
+		return true
+	case "StartWithContext":
+		if recipe.argumentsAlreadyInstrumented(call.Args, sensorVar) {
+			return false
+		}
+
+		call.Args[1] = recipe.instrumentationCallExpr("NewHandler", call.Args[1], sensorVar)
+
+		return true
+	default:
+		return false
+	}
+}
+
+// argumentsAlreadyInstrumented returns two parameters
+func (recipe *Lambda) argumentsAlreadyInstrumented(args []ast.Expr, sensorVar string) bool {
+	sensorFound := false
+	for index := range args {
+		ast.Inspect(args[index], func(node ast.Node) bool {
+			if ident, ok := (node).(*ast.Ident); ok {
+				if ident.Name == sensorVar {
+					sensorFound = true
+
+					return false
+				}
+			}
+
+			return true
+		})
+
+		if sensorFound {
+			return true
+		}
+	}
+
+	return false
+}
+
+// generate instana instrumentation expression
+func (recipe *Lambda) instrumentationCallExpr(funcName string, handler ast.Expr, sensorVar string) ast.Expr {
+	return &ast.CallExpr{
+		Fun: &ast.SelectorExpr{
+			X:   ast.NewIdent(recipe.InstanaPkg),
+			Sel: ast.NewIdent(funcName),
+		},
+		Args: []ast.Expr{
+			handler,
+			ast.NewIdent(sensorVar),
+		},
+	}
+}
---
+++ recipes/lambda_test.go
@@ -0,0 +1,185 @@
+// (c) Copyright IBM Corp. 2022
+
+package recipes_test
+
+import (
+	"bytes"
+	"github.com/instana/go-instana/recipes"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"testing"
+)
+
+func TestLambda(t *testing.T) {
+
+	examples := map[string]struct {
+		TargetPkg string
+		Code      string
+		Expected  string
+	}{
+		"lambda.Start": {
+			TargetPkg: "lambda",
+			Code: `package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(func() {})
+}
+`,
+			Expected: `package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	instalambda "github.com/instana/go-sensor/instrumentation/instalambda"
+)
+
+func main() {
+	lambda.Start(instalambda.NewHandler(func() {
+	}, __instanaSensor))
+}
+`,
+		},
+		"lambda.StartHandler": {
+			TargetPkg: "lambda",
+			Code: `package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.StartHandler(lambda.NewHandler(func() {}))
+}
+`,
+			Expected: `package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	instalambda "github.com/instana/go-sensor/instrumentation/instalambda"
+)
+
+func main() {
+	lambda.StartHandler(instalambda.WrapHandler(lambda.NewHandler(func() {
+	}), __instanaSensor))
+}
+`,
+		},
+		"lambda.StartHandlerWithContext": {
+			TargetPkg: "lambda",
+			Code: `package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+
+)
+
+func main() {
+	lambda.StartHandlerWithContext(context.Background(), lambda.NewHandler(func() {}))
+}
+`,
+			Expected: `package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+	instalambda "github.com/instana/go-sensor/instrumentation/instalambda"
+)
+
+func main() {
+	lambda.StartHandlerWithContext(context.Background(), instalambda.WrapHandler(lambda.NewHandler(func() {
+	}), __instanaSensor))
+}
+`,
+		},
+		"lambda.StartWithOptions": {
+			TargetPkg: "lambda",
+			Code: `package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.StartWithOptions(func() {}, lambda.WithContext(context.Background()))
+}
+`,
+			Expected: `package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+	instalambda "github.com/instana/go-sensor/instrumentation/instalambda"
+)
+
+func main() {
+	lambda.StartWithOptions(instalambda.NewHandler(func() {
+	}, __instanaSensor), lambda.WithContext(context.Background()))
+}
+`,
+		},
+		"lambda.StartWithContext": {
+			TargetPkg: "lambda",
+			Code: `package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.StartWithContext(context.Background(), func() {})
+}
+`,
+			Expected: `package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+	instalambda "github.com/instana/go-sensor/instrumentation/instalambda"
+)
+
+func main() {
+	lambda.StartWithContext(context.Background(), instalambda.NewHandler(func() {
+	}, __instanaSensor))
+}
+`,
+		},
+	}
+
+	assertInstrumentation(t, examples)
+}
+
+func assertInstrumentation(t *testing.T, examples map[string]struct {
+	TargetPkg string
+	Code      string
+	Expected  string
+}) {
+	for name, example := range examples {
+
+		t.Run(name, func(t *testing.T) {
+			fset := token.NewFileSet()
+			node, err := parser.ParseFile(fset, "test", example.Code, parser.AllErrors)
+
+			require.NoError(t, err)
+
+			instrumented, changed := recipes.NewLambda().
+				Instrument(token.NewFileSet(), node, example.TargetPkg, "__instanaSensor")
+
+			assert.True(t, changed)
+
+			buf := bytes.NewBuffer(nil)
+			require.NoError(t, format.Node(buf, token.NewFileSet(), instrumented))
+
+			assert.Equal(t, example.Expected, buf.String())
+		})
+
+	}
+}
