5de78b9721b80ce8d8b377b1dae72c7ae52f91ef
Willian Carvalho <willian.carvalho@instana.com>
2022-06-23 08:17:05 +0200

    Fixed main test

--- main_test.go
+++ main_test.go
@@ -23,63 +23,29 @@ func TestInstrument(t *testing.T) {
 	originalCode := `package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/julienschmidt/httprouter"
 )
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
-}
-
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
-
 func main() {
-
 	gin.New()
 
 	router := httprouter.New()
-	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
-
-	log.Fatal(http.ListenAndServe(":8080", router))
 }
 `
 
 	instrumentedCode := `package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 	instagin "github.com/instana/go-sensor/instrumentation/instagin"
 	instahttprouter "github.com/instana/go-sensor/instrumentation/instahttprouter"
 	"github.com/julienschmidt/httprouter"
 )
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
-}
-
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
-
 func main() {
 	instagin.New(__instanaSensor)
 
 	router := instahttprouter.Wrap(httprouter.New(), __instanaSensor)
-	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
-
-	log.Fatal(http.ListenAndServe(":8080", router))
 }
 `
 
